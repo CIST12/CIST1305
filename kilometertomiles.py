addeaac1c22d1ac49e13ed874f575ffddac29bcc
Swati bhardwaj <143733954+Swatibhardwaj12@users.noreply.github.com>
2023-09-01 11:10:45 -0400

    Create kilometertomiles.py

---
+++ kilometertomiles.py
@@ -0,0 +1,13 @@
+def main():
+    km=float(input("How many Kilometer? "))
+    conversion(km)
+    
+    
+
+def conversion(km):
+    miles=km * 0.6214
+    print("That is ", miles, " in miles")
+    
+    
+    
+main()
