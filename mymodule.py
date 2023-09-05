82ab6cab564e292885c720993aaaa44c4b3f595f
Swati bhardwaj <143733954+Swatibhardwaj12@users.noreply.github.com>
2023-09-04 23:01:10 -0400

    Create mymodule.py

---
+++ mymodule.py
@@ -0,0 +1,14 @@
+def main ():
+    print ("Enter Integer a, Integer b, Integer c")
+    int ("input a number a,")
+    int ("input a number b,")
+    int ("input a number c,")
+    
+
+def myModule ():
+    print ("a is 3")
+    print ("b is 2")
+    print ("c is 1")
+    
+    
+myModule ()
