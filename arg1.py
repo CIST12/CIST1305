ad1beb1b7aec85e6685597259cfd28e231e09f38
Swati bhardwaj <143733954+Swatibhardwaj12@users.noreply.github.com>
2023-09-04 23:13:32 -0400

    Create arg1.py

---
+++ arg1.py
@@ -0,0 +1,14 @@
+def main():
+    print ("Enter Integer age,Real income, String name")
+    int("your age")
+    int("your income")
+    int("your name")
+    
+    
+def main():
+    print("age is 12")
+    print("income is 1100.46")
+    print("name is Shruti")
+    
+    
+main()
