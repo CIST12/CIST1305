b88ad2f2a9b65c153593d8e904d6d320f4469090
Swati bhardwaj <143733954+Swatibhardwaj12@users.noreply.github.com>
2023-09-27 13:42:06 -0400

    wedpractice
    Snellville Library

---
+++ wed27prac1/prac1_wed27.py
@@ -0,0 +1,7 @@
+def timeS (time):
+    print("The time is, ", time,"am")
+
+
+time = input("Enter the time: ")
+timeS(time)
+
---
+++ wed27prac1/prac2_wed27.py
@@ -0,0 +1,13 @@
+def yourname():
+    entername = input("what is your name: ")
+    return entername
+
+def printname():
+    print(yourname())
+
+
+printname()
+
+
+
+
