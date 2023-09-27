537a654c0ba77261904709ac69a7a0dc2cac3781
Swati bhardwaj <143733954+Swatibhardwaj12@users.noreply.github.com>
2023-09-27 14:53:33 -0400

    Add files via upload
    chapter 4 pg 158

---
+++ wed27prac1/prac4_wed27.py
@@ -0,0 +1,9 @@
+# define a function to mulitply a num by 2
+def doubleNumber(intval):
+    result = intval * 2
+    print(result)
+
+# get input from the user
+getnum = int(input("enter number: "))
+
+doubleNumber(getnum)
